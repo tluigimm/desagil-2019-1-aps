f46d1899b49f1436f0e32554cebb09c1dd3e8e15
luvi01 <38668635+luvi01@users.noreply.github.com>
2019-04-15 23:15:46 -0300

    Fim

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -1,22 +1,32 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
 import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Light;
 import br.pro.hashi.ensino.desagil.aps.model.Switch;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
-public class GateView extends JPanel implements ItemListener {
+public class GateView extends FixedPanel implements ItemListener, MouseListener {
     private final Switch[] switches;
     private final Gate gate;
 
     private final JCheckBox[] inputBoxes;
     private final JCheckBox outputBox;
 
-    public GateView(Gate gate) {
-        this.gate = gate;
+    private final Image image;
+    private Light light;
+    private Color color;
 
+    public GateView(Gate gate) {
+        super(245, 346);
+        this.gate = gate;
+        light = new Light();
         int inputSize = gate.getInputSize();
 
         switches = new Switch[inputSize];
@@ -25,7 +35,6 @@ public class GateView extends JPanel implements ItemListener {
         for (int i = 0; i < inputSize; i++) {
             switches[i] = new Switch();
             inputBoxes[i] = new JCheckBox();
-
             gate.connect(i, switches[i]);
         }
 
@@ -34,22 +43,44 @@ public class GateView extends JPanel implements ItemListener {
         JLabel inputLabel = new JLabel("Input");
         JLabel outputLabel = new JLabel("Output");
 
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        add(inputLabel);
-        for (JCheckBox inputBox : inputBoxes) {
-            add(inputBox);
+
+        //add(inputLabel, 10, 10, 75, 25);
+        //add(outputLabel, 30, 10, 75, 25);
+
+        //add(inputLabel);
+        //System.out.println(inputSize);
+        if (inputSize > 1) {
+            add(inputBoxes[0],30, 132, 150, 25);
+            add(inputBoxes[1],30, 222, 150, 25);
         }
-        add(outputLabel);
-        add(outputBox);
+        else{
+            add(inputBoxes[0],30, 177, 150, 25);
+        }
+
+
+        //add(outputLabel,10, 311, 75, 25);
+        add(outputBox,85, 311, 120, 25);
 
         for (JCheckBox inputBox : inputBoxes) {
             inputBox.addItemListener(this);
         }
 
         outputBox.setEnabled(false);
+        String name = gate.toString() + ".png";
+        //System.out.print(gate.toString());
+
+        URL url = getClass().getClassLoader().getResource(name);
+        //System.out.print(url);
+        image = getToolkit().getImage(url);
+
+        light.setR(255);
+        light.setG(0);
+        light.setB(0);
+        addMouseListener(this);
 
         update();
+        System.out.println(gate.read());
     }
 
     private void update() {
@@ -64,10 +95,94 @@ public class GateView extends JPanel implements ItemListener {
         boolean result = gate.read();
 
         outputBox.setSelected(result);
+        int r = light.getR();
+        int g = light.getG();
+        int b = light.getB();
+
+        color = new Color(r, g, b);
+
+        this.light.connect(0,gate);
+
+        repaint();
+
     }
 
     @Override
     public void itemStateChanged(ItemEvent event) {
         update();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+
+        // Descobre em qual posição o clique ocorreu.
+        int x = event.getX();
+        int y = event.getY();
+
+        // Se o clique foi dentro do quadrado colorido...
+        if (Math.pow((x-(180+10.5)),2) + Math.pow((y-(177+10.5)),2) < Math.pow(25/2,2)){
+
+            // ...então abrimos a janela seletora de cor...
+            color = JColorChooser.showDialog(this, null, color);
+            if (color != null){
+
+                light.setR(color.getRed());
+                light.setG(color.getGreen());
+                light.setB(color.getBlue());
+            }
+            // ...e chamamos repaint para atualizar a tela.
+            repaint();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação de pressionar
+        // um botão do mouse, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação de soltar
+        // um botão do mouse, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação do mouse
+        // entrar no painel, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação do mouse
+        // sair do painel, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        // Não podemos esquecer desta linha, pois não somos os
+        // únicos responsáveis por desenhar o painel, como era
+        // o caso nos Desafios. Agora é preciso desenhar também
+        // componentes internas, e isso é feito pela superclasse.
+        super.paintComponent(g);
+
+        // Desenha a imagem, passando sua posição e seu tamanho.
+        g.drawImage(image, 10, 80, 221, 221, this);
+
+        // Desenha um quadrado cheio.
+
+        g.setColor(color);
+        g.fillOval(180, 177, 25, 25);
+
+        // Linha necessária para evitar atrasos
+        // de renderização em sistemas Linux.
+        getToolkit().sync();
     }
 }
